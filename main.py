d0fd029066dcb96870279bcf62095bc5937327e9
Javier Bravo <javier.bravolopez@agente.ocaso.es>
2025-01-26 23:29:08 +0100

    Agregado operationId al endpoint /

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/", operation_id="get_root")
 def read_root():
     return {"message": "¡Tu backend está funcionando correctamente!"}
